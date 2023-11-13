78f77a1d270203ed34cf6d9eb87ed48e5886d3a4
pp <pijiaxin@wsdashi.com>
2023-11-13 14:47:24 +0800

    feat: gitignore

--- src/factory.ts
+++ src/factory.ts
@@ -1,13 +1,20 @@
-import { OptionsConfig } from "./types";
+import fs from "fs";
+import { ConfigItem, OptionsConfig } from "./types";
 import gitignore from 'eslint-config-flat-gitignore'
 
 export function Zeus(options:OptionsConfig = {}){
   const {gitignore:enableGitignore = true} =options
 
-  const configs:any = []
+  const configs:ConfigItem[][] = []
 
   if(enableGitignore){
-    configs.push([gitignore()])
+    if(typeof enableGitignore !== 'boolean'){
+    configs.push([gitignore(enableGitignore)])
+    }else{
+      if(fs.existsSync('.gitignore')){
+        configs.push([gitignore()])
+      }
+    }
   }
 
   return configs.flat()
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,23 @@
 
-export type ConfigItem = {}
+import { FlatGitignoreOptions } from 'eslint-config-flat-gitignore'
+import { FlatESLintConfigItem } from 'eslint-define-config'
+
+export type ConfigItem = Omit<FlatESLintConfigItem,'plugins'>& {
+  /**
+   * name of config
+   */
+  name?:string
+
+  /**
+   * eslint plugins
+   */
+  plugins?:Record<string,any>
+}
 
 export interface OptionsConfig{
 
   /**
    * gitignore support
    */
-  gitignore?: boolean;
+  gitignore?: boolean|FlatGitignoreOptions
 }
